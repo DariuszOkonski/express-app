9748097bdad308f47f812dd423937884c219e718
Dariusz Okoński <darek200180@gmail.com>
2025-08-14 08:09:27 +0200

    move style and picture to public folder

--- server.js
+++ server.js
@@ -7,34 +7,35 @@ app.use((req, res, next) => {
   res.show = (name) => {
     res.sendFile(path.join(__dirname, `/views/${name}`));
   };
-
   next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
-  return res.show('index.html');
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  return res.show('about.html');
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  return res.show('contact.html');
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  return res.show('info.html');
+  res.show('info.html');
 });
 
-app.get('/history', (req, res) => {
-  return res.show('history.html');
+app.get('/history', (req, res, next) => {
+  res.show('history.html');
 });
 
 app.use((req, res) => {
-  return res.status(404).send('404 not found...');
+  res.status(404).send('404 not found...');
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000...');
+  console.log('Server is running on port: 8000');
 });
