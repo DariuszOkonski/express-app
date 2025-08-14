9a2210a16a7a8170939f5f4c47b3fa7ce5571e3a
Dariusz Okoński <darek200180@gmail.com>
2025-08-14 07:50:14 +0200

    add 404

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get('/history', (req, res) => {
   return res.show('history.html');
 });
 
+app.use((req, res) => {
+  return res.status(404).send('404 not found...');
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000...');
 });
